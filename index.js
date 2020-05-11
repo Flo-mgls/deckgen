5da431a4ed2b153861bf260842a7c03f6035d942
Florian Magalhaes <m.f.pro@outlook.com>
2020-05-11 02:04:50 +0200

    Ajour des propriétés arena et isUsed(prit en compte lors de la création de deck)

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let cardsNumberInDeck = 1; // Nombre de cartes dans un deck
+let cardsNumberInDeck = 2; // Nombre de cartes dans un deck
 let allCards = []; // Array contenant toutes les cartes du jeu
 let currentDeck = []; // Array contenant les cartes du deck actuel
 let modeDeckSelection = 0; // Mode de sélection: 0 > yolo / 
 
 // Conctructeur des cartes 
-function Cards(id, name, rarety, type, cost, image) { 
+function Cards(id, name, rarety, type, cost, image, arena, isUsed) { 
 	this.id = id,
 	this.name = name,
 	this.rarety = rarety,
 	this.type = type,
 	this.cost = cost,
-	this.image = image
+	this.image = image,
+	this.arena = arena,
+	this.isUsed = isUsed
+
 }
 // Ajout d'une méthode dans le prototype de Cards: Ajoute la carte à l'array allCards
 Cards.prototype.addCard = function() {
@@ -18,12 +21,13 @@ Cards.prototype.addCard = function() {
 }
 
 // Création des cartes avec le constructeur Cards et ajout une à une dans l'array allCards
-let prince = new Cards(1, "prince", "epic", "troup", 5, "images/prince.jpg").addCard();
-let princesse = new Cards(2, "princesse", "legendary", "troup", 3, "images/princesse.jpg").addCard();
+let prince = new Cards(1, "prince", "epic", "troup", 5, "images/prince.jpg", 1, false).addCard();
+let princess = new Cards(2, "princess", "legendary", "troup", 3, "images/princess.jpg", 2, false).addCard();
+let fireball = new Cards(3, "fireball", "rare", "spell", 4, "images/fireball.jpg", 2, false).addCard();
 
 // Donne un aléatoire selon le mode de sélection choisi
-function getRandom(){
-	switch(modeDeckSelection){
+function getRandom(mode){
+	switch(mode){
 		case 0:
 		return Math.floor(Math.random() * allCards.length);
 		break;
@@ -34,19 +38,32 @@ function getRandom(){
 }
 
 // Mode de sélection 0 - choisi 8 cartes totalement aléatoire
-function madeDeck(){
-	for(let i = 0; i < cardsNumberInDeck; i++){
-		randomCard = getRandom();
-		currentDeck.push(allCards[randomCard]);
+function madeDeckYolo(){
+	modeDeckSelection = 0;
+	while(currentDeck.length < cardsNumberInDeck){
+		randomCard = getRandom(modeDeckSelection);
+		if(allCards[randomCard].isUsed){
+			continue;
+		}else{
+			allCards[randomCard].isUsed = true;
+			currentDeck.push(allCards[randomCard]);
+		}
 	}
 }
 
 // Affichage des 8 cartes dans le DOM
 function displayDeck(){
 	currentDeck = [];
-	madeDeck();
-	document.getElementById("name").textContent = `nom: ${currentDeck[0].name}`;
-	document.getElementById("image").setAttribute("src", currentDeck[0].image);
+	for(card of allCards){
+		card.isUsed = false;
+	}
+	madeDeckYolo();
+	let cardNumber = 0;
+	let slots = document.getElementsByClassName("image");
+	for(slot of slots){
+		slot.setAttribute("src", currentDeck[cardNumber].image);
+		cardNumber++;
+	}
 }
 
 document.querySelector("button").addEventListener("click", displayDeck);
